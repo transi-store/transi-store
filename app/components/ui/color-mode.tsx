a7b790f0b08647929883025e1b4a2dffabe27bd0
Julien Deniau <1398469+jdeniau@users.noreply.github.com>
2026-02-06 22:54:14 +0100

    Update color-mode.tsx

--- app/components/ui/color-mode.tsx
+++ app/components/ui/color-mode.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { IconButtonProps } from "@chakra-ui/react";
 import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react";
 import { ThemeProvider, useTheme } from "next-themes";
