d6966ac48fc9903b85a925cacf79c9a75b5f8f55
Julien Deniau <julien.deniau@gmail.com>
2026-02-16 10:59:40 +0100

    Fix cli on windows

--- scripts/create-json-schema.mjs
+++ scripts/create-json-schema.mjs
@@ -1,12 +1,13 @@
 import { writeFileSync } from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import z from "zod";
 import schema from "@transi-store/cli/schema";
 
 const schemaJson = z.toJSONSchema(schema);
 
 // get current file dir
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const outputPath = path.resolve(__dirname, "../packages/cli/dist/schema.json");
 
