bcf0c1d1f0f1e1cbbd553c88c75511ccf4990489
Julien Deniau <julien.deniau@gmail.com>
2026-03-22 21:39:03 +0000

    add german trasnlations

---
+++ app/locales/de/index.ts
@@ -0,0 +1,4 @@
+import type { ResourceLanguage } from "i18next";
+import translation from "./translation.json" with { type: "json" };
+
+export default { translation } satisfies ResourceLanguage;
--- app/locales/index.ts
+++ app/locales/index.ts
@@ -2,5 +2,6 @@ import type { Resource } from "i18next";
 import en from "./en";
 import fr from "./fr";
 import es from "./es";
+import de from "./de";
 
-export default { en, fr, es } satisfies Resource;
+export default { en, fr, es, de } satisfies Resource;
